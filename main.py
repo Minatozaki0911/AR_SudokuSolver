853ce1d935da381fcab485e1501012f1dde67e4d
Khang Thai <48411041+Minatozaki0911@users.noreply.github.com>
2021-11-07 17:30:31 +0700

    Read image feature

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import Solver
 import cv2
+import Solver
 
 if __name__ == '__main__':
     counter = 0
-    capture_path = './test.mp4'
-    vid = cv2.VideoCapture(capture_path)
-    
-    while(vid.isOpened()):
-        ret, frame = vid.read()
-        counter += 1 
-        if counter == cv2.CAP_PROP_FRAME_COUNT:
-            counter = 0
-            vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        frame = cv2.resize(frame, (0,0), fx=0.6, fy=0.6)
-        frame = cv2.bilateralFilter(frame, 5,  50, 100)
-
-        output = Solver.solve(frame)
-        print('Output updated ', counter)
-        cv2.imshow('Output window', output)
+    capture_path = './test.jpg'
+    if capture_path.split('.')[-1]=='mp4':
+        vid = cv2.VideoCapture(capture_path)
         
-        if cv2.waitKey(1)==ord('q'):
-            break
+        while(vid.isOpened()):
+            ret, frame = vid.read()
+            counter += 1 
+            if counter == cv2.CAP_PROP_FRAME_COUNT:
+                counter = 0
+                vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            frame = cv2.resize(frame, (0,0), fx=0.6, fy=0.6)
+            frame = cv2.bilateralFilter(frame, 5,  50, 100)
+
+            output = Solver.solve(frame)
+            print('Output updated ', counter)
+            cv2.imshow('Output window', output)
+
+            if cv2.waitKey(1)==ord('q'):
+                break
+    else:
+        frame = cv2.imread(capture_path)
+        output = Solver.solve(frame)
+        print("Solved ez clap")
+        cv2.imshow('Output window', output)
 
     vid.release()
     cv2.destroyAllWindows()
