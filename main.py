674d8fbda58333536d26c6c1d09c42bdf276391d
Khang Thai <48411041+Minatozaki0911@users.noreply.github.com>
2021-11-07 18:19:52 +0700

    Add looping video featire

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import Solver
 if __name__ == '__main__':
     counter = 0
     capture_path = './test.mp4'
+
     if capture_path.split('.')[-1]=='mp4':
         vid = cv2.VideoCapture(capture_path)
-        
         while(vid.isOpened()):
             ret, frame = vid.read()
-            #counter += 1
-            #if counter == cv2.CAP_PROP_FRAME_COUNT:
-            #    counter = 0
-            #    vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            counter += 1
+            if counter == vid.get(cv2.CAP_PROP_FRAME_COUNT):
+                counter = 0
+                vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
             frame = cv2.resize(frame, (0,0), fx=0.6, fy=0.6)
             frame = cv2.bilateralFilter(frame, 5,  50, 100)
             cv2.imshow('Input', frame)
